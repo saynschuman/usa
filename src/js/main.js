c000cd893c0ac7ece73d3a39bcacf68a8bb04071
saynschuman <saynschuman@gmail.com>
2017-04-11 17:52:10 +0300

    usa finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,33 @@
 (function($) {
     
+if ($(".top-slider_2").length) {
 
+	$(".top-slider_2").owlCarousel({
+		loop:false,
+		margin:0,
+		autoplay: false,
+		autoplayHoverPause: false,
+		smartSpeed: 500,
+		// stagePadding: 65,
+		lazyContent: true,
+		nav:true,
+		dots:true,
+		navText: ["", ""],
+		responsive:{
+			0:{
+				items:1
+			}
+		}
+	});
+};
+
+// $(".top-slider_2 .owl-stage:first-child").css('padding-left', '0');
+
+// $('.owl-next').click(function(){
+// 	if ($('owl-next').hasClass('disabled')) {
+// 		$(".top-slider_2 .owl-stage:first-child").css('padding-right', '0');
+// 	}
+// })
 
 })(jQuery);
+
